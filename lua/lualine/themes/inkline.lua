f0a405b89d9e12b8f4ce5e42103f8c68c278d570
Nicholas deLannoy <ndelannoy@paypal.com>
2025-05-05 17:53:53 +0000

    tweak lualine colors

--- lua/lualine/themes/inkline.lua
+++ lua/lualine/themes/inkline.lua
@@ -7,27 +7,40 @@ function M.get()
   local hl = {}
 
   hl.normal = {
-    a = { fg = colors.orange, bg = colors.bg },
-    b = { fg = colors.cyan, bg = colors.bg },
+    a = { fg = colors.black, bg = colors.orange },
+    b = { fg = colors.orange, bg = colors.fg_gutter },
     c = { fg = colors.fg, bg = colors.bg },
     x = { fg = colors.fg, bg = colors.bg },
-    y = { fg = colors.cyan, bg = colors.bg },
-    z = { fg = colors.grey, bg = colors.bg },
+    y = { fg = colors.orange, bg = colors.fg_gutter },
+    z = { fg = colors.black, bg = colors.orange },
   }
 
   hl.insert = {
-    a = { fg = colors.green, bg = colors.bg },
-    z = { fg = colors.grey, bg = colors.bg },
+    a = { fg = colors.black, bg = colors.green },
+    b = { fg = colors.green, bg = colors.fg_gutter },
+    y = { fg = colors.green, bg = colors.fg_gutter },
+    z = { fg = colors.black, bg = colors.green },
+  }
+
+  hl.command = {
+    a = { fg = colors.black, bg = colors.gold },
+    b = { fg = colors.gold, bg = colors.fg_gutter },
+    y = { fg = colors.gold, bg = colors.fg_gutter },
+    z = { fg = colors.black, bg = colors.gold },
   }
 
   hl.visual = {
-    a = { fg = colors.magenta, bg = colors.bg },
-    z = { fg = colors.grey, bg = colors.bg },
+    a = { fg = colors.black, bg = colors.magenta },
+    b = { fg = colors.magenta, bg = colors.fg_gutter },
+    y = { fg = colors.magenta, bg = colors.fg_gutter },
+    z = { fg = colors.black, bg = colors.magenta },
   }
 
   hl.terminal = {
-    a = { fg = colors.orange, bg = colors.bg },
-    z = { fg = colors.grey, bg = colors.bg },
+    a = { fg = colors.black, bg = colors.teal },
+    b = { fg = colors.teal, bg = colors.fg_gutter },
+    y = { fg = colors.teal, bg = colors.fg_gutter },
+    z = { fg = colors.black, bg = colors.teal },
   }
 
   return hl
