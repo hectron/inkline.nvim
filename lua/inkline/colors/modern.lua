381a978c31b2eee5f94d3dca17dc3ce87c32f706
Hector Rios <that.hector@gmail.com>
2025-07-08 13:52:02 -0500

    feat(modern): slightly bump up the reds

--- lua/inkline/colors/modern.lua
+++ lua/inkline/colors/modern.lua
@@ -7,6 +7,7 @@ M.colors = vim.tbl_deep_extend("force", base.base, {
   -- Variant-specific syntax colors
   function_color = "#3498db", -- Blue for functions
   constant_color = "#ffbd5e", -- Bright orange for constants
+  keyword = "#ff4757",        -- Slightly more saturated red for keywords
 })
 
 return M
