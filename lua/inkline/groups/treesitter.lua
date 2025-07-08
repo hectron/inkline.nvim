f59c5e1f6289692574a5b266c10385955bb2d8ec
Hector Rios <that.hector@gmail.com>
2025-07-08 00:17:28 -0500

    feat: Add better colors for gitcommit
    This makes it a lot easier to udnerstand what was changed.

--- lua/inkline/groups/treesitter.lua
+++ lua/inkline/groups/treesitter.lua
@@ -24,9 +24,9 @@ function M.get(c, opts)
     ["@constant.builtin"]             = "Special",
     ["@constant.macro"]               = "Define",
     ["@constructor"]                  = { fg = c.purple_3 }, -- For constructor calls and definitions: `= { }` in Lua, and Java constructors.
-    ["@diff.delta"]                   = "DiffChange",
-    ["@diff.minus"]                   = "DiffDelete",
-    ["@diff.plus"]                    = "DiffAdd",
+    ["@diff.delta"]                   = { fg = c.yellow, bold = true },
+    ["@diff.minus"]                   = { fg = c.red, bold = true },
+    ["@diff.plus"]                    = { fg = c.green, bold = true },
     ["@function"]                     = { fg = c.function_color },
     ["@function.builtin"]             = { fg = c.function_color, bold = true },
     ["@function.call"]                = { fg = c.function_color },
@@ -59,15 +59,16 @@ function M.get(c, opts)
     ["@markup.heading.4.markdown"]    = { fg = c.header_4, bold = true },
     ["@markup.heading.5.markdown"]    = { fg = c.header_5, bold = true },
     ["@markup.heading.6.markdown"]    = { fg = c.header_6, bold = true },
+    ["@markup.heading.gitcommit"]     = { fg = c.light_blue, bold = true, underline = true },
     ["@markup.italic"]                = { italic = true },
     ["@markup.link"]                  = { fg = c.teal },
     ["@markup.link.label"]            = "SpecialChar",
     ["@markup.link.label.symbol"]     = "Identifier",
     ["@markup.link.url"]              = "Underlined",
     ["@markup.list"]                  = { fg = c.blue_green_2 }, -- For special punctutation that does not fall in the categories before.
-    ["@markup.list.checked"]          = { fg = c.green },      -- For brackets and parens.
+    ["@markup.list.checked"]          = { fg = c.green },        -- For brackets and parens.
     ["@markup.list.markdown"]         = { fg = c.orange_2, bold = true },
-    ["@markup.list.unchecked"]        = { fg = c.light_blue }, -- For brackets and parens.
+    ["@markup.list.unchecked"]        = { fg = c.light_blue },   -- For brackets and parens.
     ["@markup.math"]                  = "Special",
     ["@markup.raw"]                   = "String",
     ["@markup.raw.markdown_inline"]   = { bg = c.bg_highlight, fg = c.light_orange },
@@ -82,14 +83,14 @@ function M.get(c, opts)
     ["@number.float"]                 = "Float",
     ["@operator"]                     = { fg = Util.blend_fg(c.blue_green_2, 0.6) }, -- For any operator: `+`, but also `->` and `*` in C.
     ["@property"]                     = { fg = c.purple_2 },
-    ["@punctuation.bracket"]          = { fg = c.dark_white },                     -- For brackets and parens.
-    ["@punctuation.delimiter"]        = { fg = c.blue_green_2 },                   -- For delimiters ie: `.`
-    ["@punctuation.special"]          = { fg = c.blue_green_2 },                   -- For special symbols (e.g. `{}` in string interpolation)
-    ["@punctuation.special.markdown"] = { fg = c.orange },                         -- For special symbols (e.g. `{}` in string interpolation)
+    ["@punctuation.bracket"]          = { fg = c.dark_white },                       -- For brackets and parens.
+    ["@punctuation.delimiter"]        = { fg = c.blue_green_2 },                     -- For delimiters ie: `.`
+    ["@punctuation.special"]          = { fg = c.blue_green_2 },                     -- For special symbols (e.g. `{}` in string interpolation)
+    ["@punctuation.special.markdown"] = { fg = c.orange },                           -- For special symbols (e.g. `{}` in string interpolation)
     ["@string"]                       = { fg = c.string_color },
     ["@string.documentation"]         = { fg = c.string_color, italic = true },
     ["@string.escape"]                = { fg = c.purple_2 }, -- For escape characters within a string.
-    ["@string.regexp"]                = { fg = c.cyan },   -- For regexes.
+    ["@string.regexp"]                = { fg = c.cyan },     -- For regexes.
     ["@tag"]                          = { fg = c.keyword },
     ["@tag.attribute"]                = { fg = c.purple_2 },
     ["@tag.delimiter"]                = { fg = c.blue_green_2 },
@@ -97,11 +98,16 @@ function M.get(c, opts)
     ["@type.ruby"]                    = { fg = c.type_color },
     ["@type.definition"]              = { fg = c.type_color },
     ["@type.qualifier"]               = { fg = c.keyword },
-    ["@variable"]                     = { fg = c.variable_color },               -- Any variable name that does not have another highlight.
-    ["@variable.builtin"]             = { fg = c.pink, italic = true },          -- Variable names that are defined by the languages, like `this` or `self`.
-    ["@variable.parameter"]           = { fg = c.blue_green },                   -- For parameters of a function.
+    ["@variable"]                     = { fg = c.variable_color },                 -- Any variable name that does not have another highlight.
+    ["@variable.builtin"]             = { fg = c.pink, italic = true },            -- Variable names that are defined by the languages, like `this` or `self`.
+    ["@variable.parameter"]           = { fg = c.blue_green },                     -- For parameters of a function.
     ["@variable.parameter.builtin"]   = { fg = Util.blend_fg(c.blue_green, 0.8) }, -- For builtin parameters of a function, e.g. "..." or Smali's p[1-99]
     ["@variable.parameter.lua"]       = { fg = c.pink },
+
+    -- Git commit specific highlights
+    ["@comment.gitcommit"]            = { fg = opts.purple_comments and c.purple or c.dark_grey_5, italic = true },
+    ["@string.gitcommit"]             = { fg = c.string_color },
+    ["@keyword.gitcommit"]            = { fg = c.keyword },
   }
 
   return ret
